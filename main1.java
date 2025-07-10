04e91d3c4997c1fe62b7ad7ac9e4c56ea0d7daa5
Kavya4615 <kavyajain4615@gmail.com>
2025-07-10 21:33:37 +0530

    circle class

--- main1.java
+++ main1.java
@@ -1,6 +1,15 @@
 public class main1{
     public static void main(String[] args) {
         System.out.println("hello");
+        circle q = new circle();
+        System.out.println(q.getarea(5));
     
     }
+}
+class circle{
+    int area;
+    public double getarea(int radius){
+        return 3.14*radius*radius;
+    }
+
 }
